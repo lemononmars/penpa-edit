b082c89f6f370790beb71195ac51214063879d46
lemononmars <lemon_on_mars@hotmail.com>
2026-08-15 15:10:33 +0700

    second fix

--- docs/src/main.ts
+++ docs/src/main.ts
@@ -1,8 +1,18 @@
 import { mount } from "svelte";
 import App from "./App.svelte";
+import BattleApp from "./BattleApp.svelte";
 
-document.documentElement.classList.add("svelte-home");
-mount(App, { target: document.getElementById("svelte-app")! });
-window.requestAnimationFrame(() => {
-    document.documentElement.classList.add("svelte-mounted");
-});
+const isBattleDomain = (location.hostname.includes("sudokubattle") || location.hostname.includes("sudoku-battle")) && !location.search.includes("embed=1");
+
+if (isBattleDomain) {
+    document.title = "Sudoku Battle";
+    document.documentElement.classList.remove("svelte-home");
+    document.body.innerHTML = '<div id="battle-app"></div>';
+    mount(BattleApp, { target: document.getElementById("battle-app")! });
+} else {
+    document.documentElement.classList.add("svelte-home");
+    mount(App, { target: document.getElementById("svelte-app")! });
+    window.requestAnimationFrame(() => {
+        document.documentElement.classList.add("svelte-mounted");
+    });
+}
